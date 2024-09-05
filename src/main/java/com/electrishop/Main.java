7fdc6d3c8002a585d786a7c04d6294299c3e9b3e
SebastianDuarte13 <jsdr137@gmail.com>
2024-09-04 22:29:34 -0500

    :shipit: algo se esta horneando :bread::dash:

--- src/main/java/com/electrishop/Main.java
+++ src/main/java/com/electrishop/Main.java
@@ -1,7 +1,16 @@
 package com.electrishop;
 
+import com.electrishop.menu_principal.infrastructure.out.MenuRepository;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Crear una instancia de la clase Login
+        MenuRepository menuRepository = new MenuRepository();
+        // Mostrar la ventana del menu principal
+        menuRepository.setVisible(true);
+
+        // Ya una vez terminado el programa manda un mensajito
+        System.out.println("Programa terminado.");
     }
 }
